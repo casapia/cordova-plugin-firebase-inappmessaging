2af24eb0010ab67a0ebde83f7358a47a3446a85a
Casapia <angel.casapia@gmail.com>
2024-05-17 00:53:21 +0200

    chore: Update iOS Firebase POD source URL and version in plugin.xml

--- hooks/ios/fixpod.js
+++ hooks/ios/fixpod.js
@@ -1,27 +1,24 @@
-// const fs = require("fs");
+const fs = require("fs");
 const path = require("path");
 const execa = require("execa");
 
 module.exports = (context) => {
   const platformPath = path.resolve(context.opts.projectRoot, "platforms/ios");
-  //   const podfilePath = path.resolve(platformPath, "Podfile");
+  const podfilePath = path.resolve(platformPath, "Podfile");
 
-  //   if (!fs.existsSync(podfilePath)) {
-  //     console.log(
-  //       `'${podfilePath}' does not exist. Firebase deployment fix skipped.`
-  //     );
-  //     return;
-  //   }
+  if (!fs.existsSync(podfilePath)) {
+    console.log(
+      `'${podfilePath}' does not exist. Firebase deployment fix skipped.`
+    );
+    return;
+  }
 
-  //   let podfileContent = fs.readFileSync(podfilePath, "utf-8");
-  //   if (podfileContent.indexOf("post_install") == -1) {
-  //     podfileContent =
-  //       `pod install --repo-update
-  // ` + podfileContent;
-  //     fs.writeFileSync(podfilePath, podfileContent, "utf-8");
-  console.log("Running pod install --repo-update");
+  let podfileContent = fs.readFileSync(podfilePath, "utf-8");
+  console.log("X Fixing Podfile for Firebase InAppMessaging");
+  console.log(podfileContent);
+
+  console.log("X Running pod install --repo-update");
   return execa("pod", ["install", "--repo-update"], {
     cwd: platformPath,
   });
-  //   }
 };
