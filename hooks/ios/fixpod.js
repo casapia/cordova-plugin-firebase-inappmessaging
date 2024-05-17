877704b686f2309325a2762b2598af4d60654ce7
Casapia <angel.casapia@gmail.com>
2024-05-17 02:07:34 +0200

    Update iOS Firebase POD version and remove verbose flag in fixpod.js

--- hooks/ios/fixpod.js
+++ hooks/ios/fixpod.js
@@ -4,7 +4,7 @@ const execa = require("execa");
 
 module.exports = (context) => {
   const platformPath = path.resolve(context.opts.projectRoot, "platforms/ios");
-  const podfilePath = path.resolve(platformPath, "Podfile");
+  const podfilePath = path.resolve(platformPath, "Podfile.lock");
 
   if (!fs.existsSync(podfilePath)) {
     console.log(
@@ -14,21 +14,8 @@ module.exports = (context) => {
   }
 
   let podfileContent = fs.readFileSync(podfilePath, "utf-8");
-  if (podfileContent.indexOf("post_install") == -1) {
-    podfileContent += `
-
-post_install do |installer|
-    installer.pods_project.targets.each do |target|
-        target.build_configurations.each do |config|
-            config.build_settings.delete 'IPHONEOS_DEPLOYMENT_TARGET'
-        end
-    end
-end
-
-`;
-    fs.writeFileSync(podfilePath, podfileContent, "utf-8");
-  }
-  console.log("X Running pod install --repo-update");
+  console.log("Podfile content: ", podfileContent);
+  fs.writeFileSync(podfilePath, "", "utf-8");
   return execa("pod", ["install", "--repo-update"], {
     cwd: platformPath,
   });
